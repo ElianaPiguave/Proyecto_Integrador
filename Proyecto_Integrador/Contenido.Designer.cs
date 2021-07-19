942a03cfc5e89a45221da2e2e51ec14ea194e9de
David <David@DESKTOP-SHAL13A>
2021-07-19 18:50:12 -0500

    boton agregado

--- Proyecto_Integrador/Contenido.Designer.cs
+++ Proyecto_Integrador/Contenido.Designer.cs
@@ -31,6 +31,8 @@ namespace Proyecto_Integrador
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -42,10 +44,20 @@ namespace Proyecto_Integrador
             // 
             // panel2
             // 
+            this.panel2.Controls.Add(this.button1);
             this.panel2.Location = new System.Drawing.Point(3, 40);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(200, 583);
             this.panel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(82, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Contenido
             // 
@@ -58,6 +70,7 @@ namespace Proyecto_Integrador
             this.Name = "Contenido";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Contenido";
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -66,5 +79,6 @@ namespace Proyecto_Integrador
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
     }
 }
