03ff8b9f44c5e8b8246962def6da8becc1608216
ELIANA MAYERLI PIGUAVE YAMA <eliana.piguave.yama@estudiantes.cordillera.edu.ec>
2021-07-26 18:24:23 -0500

    login cambios

--- Proyecto_Integrador/Login.Designer.cs
+++ Proyecto_Integrador/Login.Designer.cs
@@ -32,8 +32,8 @@ namespace Proyecto_Integrador
             this.panel1 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txt_usuario = new System.Windows.Forms.TextBox();
+            this.txt_pass = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
@@ -52,16 +52,18 @@ namespace Proyecto_Integrador
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(375, 454);
+            this.panel1.Size = new System.Drawing.Size(333, 363);
             this.panel1.TabIndex = 0;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Proyecto_Integrador.Properties.Resources.WhatsApp_Image_2021_07_15_at_21_39_24_removebg_preview;
-            this.pictureBox1.Location = new System.Drawing.Point(11, 43);
+            this.pictureBox1.Location = new System.Drawing.Point(10, 34);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(355, 348);
+            this.pictureBox1.Size = new System.Drawing.Size(316, 278);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -71,35 +73,37 @@ namespace Proyecto_Integrador
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label1.Location = new System.Drawing.Point(648, 63);
+            this.label1.Location = new System.Drawing.Point(576, 50);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(133, 40);
+            this.label1.Size = new System.Drawing.Size(112, 36);
             this.label1.TabIndex = 1;
             this.label1.Text = "LOGIN";
             // 
-            // textBox1
+            // txt_usuario
             // 
-            this.textBox1.Location = new System.Drawing.Point(514, 177);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(397, 26);
-            this.textBox1.TabIndex = 2;
+            this.txt_usuario.Location = new System.Drawing.Point(457, 142);
+            this.txt_usuario.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_usuario.Name = "txt_usuario";
+            this.txt_usuario.Size = new System.Drawing.Size(353, 22);
+            this.txt_usuario.TabIndex = 2;
             // 
-            // textBox2
+            // txt_pass
             // 
-            this.textBox2.Location = new System.Drawing.Point(514, 275);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(397, 26);
-            this.textBox2.TabIndex = 3;
-            this.textBox2.UseSystemPasswordChar = true;
+            this.txt_pass.Location = new System.Drawing.Point(457, 220);
+            this.txt_pass.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_pass.Name = "txt_pass";
+            this.txt_pass.Size = new System.Drawing.Size(353, 22);
+            this.txt_pass.TabIndex = 3;
+            this.txt_pass.UseSystemPasswordChar = true;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Transparent;
-            this.label2.Location = new System.Drawing.Point(514, 146);
+            this.label2.Location = new System.Drawing.Point(457, 117);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(103, 27);
+            this.label2.Size = new System.Drawing.Size(88, 23);
             this.label2.TabIndex = 4;
             this.label2.Text = "USUARIO";
             // 
@@ -108,9 +112,9 @@ namespace Proyecto_Integrador
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Transparent;
-            this.label3.Location = new System.Drawing.Point(514, 243);
+            this.label3.Location = new System.Drawing.Point(457, 194);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(148, 27);
+            this.label3.Size = new System.Drawing.Size(127, 23);
             this.label3.TabIndex = 5;
             this.label3.Text = "CONTRASEÑA";
             // 
@@ -118,9 +122,10 @@ namespace Proyecto_Integrador
             // 
             this.button1.BackColor = System.Drawing.Color.DodgerBlue;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(637, 320);
+            this.button1.Location = new System.Drawing.Point(566, 256);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(158, 43);
+            this.button1.Size = new System.Drawing.Size(140, 34);
             this.button1.TabIndex = 6;
             this.button1.Text = "Ingresar";
             this.button1.UseVisualStyleBackColor = false;
@@ -130,9 +135,10 @@ namespace Proyecto_Integrador
             // 
             this.btn_minimizar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.btn_minimizar.Image = global::Proyecto_Integrador.Properties.Resources.mini;
-            this.btn_minimizar.Location = new System.Drawing.Point(987, 12);
+            this.btn_minimizar.Location = new System.Drawing.Point(877, 10);
+            this.btn_minimizar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_minimizar.Name = "btn_minimizar";
-            this.btn_minimizar.Size = new System.Drawing.Size(34, 35);
+            this.btn_minimizar.Size = new System.Drawing.Size(30, 28);
             this.btn_minimizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.btn_minimizar.TabIndex = 8;
             this.btn_minimizar.TabStop = false;
@@ -142,9 +148,10 @@ namespace Proyecto_Integrador
             // 
             this.btn_cerrar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.btn_cerrar.Image = global::Proyecto_Integrador.Properties.Resources.cerrar;
-            this.btn_cerrar.Location = new System.Drawing.Point(1027, 12);
+            this.btn_cerrar.Location = new System.Drawing.Point(913, 10);
+            this.btn_cerrar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_cerrar.Name = "btn_cerrar";
-            this.btn_cerrar.Size = new System.Drawing.Size(34, 35);
+            this.btn_cerrar.Size = new System.Drawing.Size(30, 28);
             this.btn_cerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.btn_cerrar.TabIndex = 7;
             this.btn_cerrar.TabStop = false;
@@ -155,30 +162,32 @@ namespace Proyecto_Integrador
             this.lkl_registro.AutoSize = true;
             this.lkl_registro.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lkl_registro.LinkColor = System.Drawing.Color.Gainsboro;
-            this.lkl_registro.Location = new System.Drawing.Point(671, 381);
+            this.lkl_registro.Location = new System.Drawing.Point(596, 305);
             this.lkl_registro.Name = "lkl_registro";
-            this.lkl_registro.Size = new System.Drawing.Size(110, 25);
+            this.lkl_registro.Size = new System.Drawing.Size(96, 20);
             this.lkl_registro.TabIndex = 9;
             this.lkl_registro.TabStop = true;
             this.lkl_registro.Text = "Registrarse";
+            this.lkl_registro.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lkl_registro_LinkClicked);
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(94)))), ((int)(((byte)(162)))));
-            this.ClientSize = new System.Drawing.Size(1073, 454);
+            this.ClientSize = new System.Drawing.Size(954, 363);
             this.Controls.Add(this.lkl_registro);
             this.Controls.Add(this.btn_minimizar);
             this.Controls.Add(this.btn_cerrar);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txt_pass);
+            this.Controls.Add(this.txt_usuario);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Login";
             this.Opacity = 0.9D;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -198,8 +207,8 @@ namespace Proyecto_Integrador
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txt_usuario;
+        private System.Windows.Forms.TextBox txt_pass;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
